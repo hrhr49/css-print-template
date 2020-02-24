c4777ca352f32b239c23e748600725043913a3fa
hrhr49 <hiro49410@gmail.com>
2020-02-24 19:39:38 +0900

    スタイルをcssからscssに変更(gulpfileも修正)。フォルダ構成変更。

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 const exec = require('child_process').exec;
 
-let build_pdf = (callback) => {
-  var command = 'vivliostyle build src/index.html'
+let scss2css = () => {
+  return gulp.src("src/scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+};
+
+let html2pdf = (callback) => {
+  var command = 'vivliostyle build src/index.html -o output/output.pdf'
   exec(command, (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
@@ -10,8 +17,19 @@ let build_pdf = (callback) => {
   });
 };
 
-gulp.task('build', build_pdf);
-gulp.task('watch', () => {
-  gulp.watch('src/**/*.html', build_pdf);
-  gulp.watch('src/**/*.css', build_pdf);
-});
+
+let build = gulp.series(scss2css, html2pdf);
+
+exports.build = build;
+exports.watch = () => {
+  gulp.watch(['src/scss/*.scss', 'src/*.html'], build);
+};
+
+
+// gulp.task('build', build_pdf);
+// gulp.task('watch', () => {
+//   gulp.watch('scss/*.scss', build_css);
+//   gulp.watch('src/**/*.html', build_pdf);
+//   gulp.watch('css/*.css', build_pdf);
+// });
+
