b4d9dca56abc9dba44227143847ab74c0183aee3
hrhr49 <hiro49410@gmail.com>
2020-03-01 16:31:58 +0900

    ブックマーク生成スクリプトの追加(TODO:抽出処理)

---
+++ add_bookmarks.py
@@ -0,0 +1,30 @@
+# 参考 
+# https://stackoverflow.com/questions/18855907/adding-bookmarks-using-pypdf2
+# https://stackoverflow.com/questions/50950825/pypdf2-why-am-i-getting-an-index-error-list-index-out-of-range
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import json
+
+def add_bookmark(bookmark_dict, pdf_writer, parent=None):
+    title = bookmark_dict['title']
+    page = bookmark_dict['page']
+    children = bookmark_dict.get('children', [])
+    current = pdf_writer.addBookmark(title, page, parent)
+    for child in children:
+        add_bookmark(child, pdf_writer, current)
+
+if __name__ == '__main__':
+    writer = PdfFileWriter()
+    reader = PdfFileReader(open('output/output.pdf', 'rb'))
+    if reader.isEncrypted:
+        reader.decrypt('')
+
+    for page in reader.pages:
+        writer.addPage(page)
+
+    parent = writer.addBookmark('Introduction', 0) # add parent bookmark
+    writer.addBookmark('Hello, World', 0, parent) # add child bookmark
+
+    bookmarks = json.load(open('bookmarks.json'))
+    for bookmark in bookmarks:
+        add_bookmark(bookmark, writer)
+    writer.write(open('output/with_bookmark.pdf', 'wb'))
